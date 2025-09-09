ece46506e0efdb3a7518f3d2ea97e10266bc2fb0
James Pecore <jjp2196@columbia.edu>
2025-09-09 14:31:34 -0400

    adding map generation

---
+++ dagster_university/dagster_essentials/src/dagster_essentials/defs/assets/metrics.py
@@ -0,0 +1,63 @@
+import dagster as dg
+
+import matplotlib.pyplot as plt
+import geopandas as gpd
+
+import duckdb
+import os
+
+from dagster_essentials.defs.assets import constants
+
+
+@dg.asset
+def metrics(context: dg.AssetExecutionContext) -> dg.MaterializeResult: ...
+
+# src/dagster_essentials/defs/assets/metrics.py
+@dg.asset(
+    deps=["taxi_trips", "taxi_zones"]
+)
+def manhattan_stats() -> None:
+
+    # SQL query joins trips and zones, filters to just trips in Manhattan,
+    # aggregates the data to get number of trips per neighborhood
+    query = """
+        select
+            zones.zone,
+            zones.borough,
+            zones.geometry,
+            count(1) as num_trips,
+        from trips
+        left join zones on trips.pickup_zone_id = zones.zone_id
+        where borough = 'Manhattan' and geometry is not null
+        group by zone, borough, geometry
+    """
+
+    conn = duckdb.connect(os.getenv("DUCKDB_DATABASE"))
+
+    # Executes the query against the DuckDB database
+    trips_by_zone = conn.execute(query).fetch_df()
+
+    trips_by_zone["geometry"] = gpd.GeoSeries.from_wkt(trips_by_zone["geometry"])
+    trips_by_zone = gpd.GeoDataFrame(trips_by_zone)
+
+    # Saves the transformed data to a GeoJSON file
+    with open(constants.MANHATTAN_STATS_FILE_PATH, 'w') as output_file:
+        output_file.write(trips_by_zone.to_json())
+
+    # src/dagster_essentials/defs/assets/metrics.py
+@dg.asset(
+    deps=["manhattan_stats"],
+)
+def manhattan_map() -> None:
+    trips_by_zone = gpd.read_file(constants.MANHATTAN_STATS_FILE_PATH)
+
+    fig, ax = plt.subplots(figsize=(10, 10))
+    trips_by_zone.plot(column="num_trips", cmap="plasma", legend=True, ax=ax, edgecolor="black")
+    ax.set_title("Number of Trips per Taxi Zone in Manhattan")
+
+    ax.set_xlim([-74.05, -73.90])  # Adjust longitude range
+    ax.set_ylim([40.70, 40.82])  # Adjust latitude range
+    
+    # Save the image
+    plt.savefig(constants.MANHATTAN_MAP_FILE_PATH, format="png", bbox_inches="tight")
+    plt.close(fig)
